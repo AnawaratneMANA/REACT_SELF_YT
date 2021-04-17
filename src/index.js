f453e667f50aae0bbe35aa023e379b9c8168e1fa
AnawaratneMANA <it19086408@my.sliit.lk>
2021-04-17 02:38:14 -0400

    Passing values via props two objects

--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ import ReactDom  from 'react-dom'
 //Adding some CSS -> Importing CSS
 import './index.css'
 
-//Creating Varibles
-const manfacture = "Acer"
-const model = "Inspiron 15"
-const img = "https://images-na.ssl-images-amazon.com/images/I/41vMYgD92xL.jpg"
+//Creating a Laptop Class
+const AcerLaptop = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/41vMYgD92xL.jpg",
+  model: "Inspiron 15",
+  manfacture: "Acer"
+}
+
+const LenovoLaptop = {
+  img: "https://m.media-amazon.com/images/I/61dZniqZ32L._AC_UY218_.jpg",
+  model: "Lenovo T480",
+  manfacture: "Lenovo"
+
+}
 
 //Adding CSS classes to the Component.
 function BookList(){
@@ -22,8 +31,8 @@ function BookList(){
     </div>
 
     <section className = "bookList">
-    <Book job='developer'/>
-    <Book title ="2nd one title"/>
+    <Book img={AcerLaptop.img} manfacture={AcerLaptop.manfacture} model ={AcerLaptop.model}/>
+    <Book img={LenovoLaptop.img} manfacture={LenovoLaptop.manfacture} model ={LenovoLaptop.model}/>
     </section>
   </list>
   );
@@ -31,19 +40,11 @@ function BookList(){
 
 //Adding different parameters to the book component
 const Book = (props) => {
-
-  //Printing Props 
-  console.log(props)
-
-  const Brand = 'Acer';
   return (
     <article className="book"> 
-      <img src = {img} alt ="image" ></img>
-      <h1>{manfacture.toUpperCase()}</h1>
-      <h4>{model} </h4>
-      <h2>{props.job}</h2>
-      <h2>{props.title}</h2>
-      {console.log(props)}
+      <img src={props.img} alt ="image"></img>
+      <h1>{props.manfacture}</h1>
+      <h4>{props.model} </h4>
     </article>
   )
 }
