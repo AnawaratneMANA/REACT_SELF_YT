b0407e10183a4aacf7ed239643e8b0d11c745b8a
AnawaratneMANA <it19086408@my.sliit.lk>
2021-04-17 04:34:39 -0400

    Importing the data object array from a external file.

---
+++ src/books.js
@@ -0,0 +1,26 @@
+export const Laptops = [
+    {
+      id: 1,
+      img: "https://images-na.ssl-images-amazon.com/images/I/41vMYgD92xL.jpg",
+      model: "Inspiron 15",
+      manfacture: "Acer",
+      status: 'In Stock'
+    },
+  
+    {
+      id: 2,
+      img: "https://m.media-amazon.com/images/I/61dZniqZ32L._AC_UY218_.jpg",
+      model: "Lenovo T480",
+      manfacture: "Lenovo",
+    },
+  
+    {
+      id: 3,
+      img: "https://m.media-amazon.com/images/I/91kzJEfoVqL._AC_UL320_.jpg",
+      model: "HP Elitebook 840",
+      manfacture: "HP",
+      status: 'Pre order'
+    },
+  
+  
+  ]
--- src/index.js
+++ src/index.js
@@ -7,37 +7,10 @@ import ReactDom from 'react-dom'
 //Adding some CSS -> Importing CSS
 import './index.css'
 
-//Creating a Laptop Class
-/*
-  In react we can iterate in a object array in one go.
-  But this is possible with string array which is much simpler form.
-*/
-const Laptops = [
-  {
-    id: 1,
-    img: "https://images-na.ssl-images-amazon.com/images/I/41vMYgD92xL.jpg",
-    model: "Inspiron 15",
-    manfacture: "Acer",
-    status: 'In Stock'
-  },
+//Importing the object array from external class
+//With JS files we don't have to mentioned the extension.
+import {Laptops} from './books'
 
-  {
-    id: 2,
-    img: "https://m.media-amazon.com/images/I/61dZniqZ32L._AC_UY218_.jpg",
-    model: "Lenovo T480",
-    manfacture: "Lenovo",
-  },
-
-  {
-    id: 3,
-    img: "https://m.media-amazon.com/images/I/91kzJEfoVqL._AC_UL320_.jpg",
-    model: "HP Elitebook 840",
-    manfacture: "HP",
-    status: 'Pre order'
-  },
-
-
-]
 
 //Adding CSS classes to the Component.
 function BookList() {
