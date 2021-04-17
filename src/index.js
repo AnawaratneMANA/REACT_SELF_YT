76c20e013666abf27d582391330f1e1e7704c034
AnawaratneMANA <it19086408@my.sliit.lk>
2021-04-17 02:02:23 -0400

    Accessing JavaScript inside JSX

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const Book = () => {
   return (
     <article className="book"> 
       <img src = "https://images-na.ssl-images-amazon.com/images/I/41vMYgD92xL.jpg" alt ="image" ></img>
-      <h1>{Brand} Inspiron Laptop</h1>
+      <h1>{Brand.toUpperCase()} {5} Inspiron Laptop</h1>
       <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Core i7 Laptops</h4>
     </article>
   )
