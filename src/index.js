d0782da309208b3fecfacb15c1cde0d5f0ea7289
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-04-17 00:53:43 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Importing react
+import React from 'react'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Importing React-DOM 
+import ReactDom  from 'react-dom'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Creating a function -> Capatilizing the class name make them compoonents.
+/*
+
+We always have to return something.
+We always need to return a single element.
+We can have any tag we want.
+Attrbutes need to be in Camel case. Otherwise error.
+We have to close every single element. (Even one with don't have closing tags -> img)
+Always use paranthesis with return statement.
+
+*/ 
+function Greeting(){
+  return ( 
+  <mainarticle>
+    <h1>Topic</h1>
+    <p>Sample Text</p>
+  </mainarticle>
+  );
+}
+
+
+//Using fragments -> we can also keep the tags empty.
+// function Greeting(){
+//   return <React.Fragment>
+//     <h1>Topic</h1>
+//     <p>Sample Text</p>
+//   </React.Fragment>
+// }
+
+//Create Element Syntax -> Not reccomend to use.
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'hello world');
+// }
+
+//Multi Tags with Create Element
+/*This can be really messy when having a lot of nested elements*/
+/*
+const Greeting = () => {
+  return React.createElement(
+  'div', 
+   {}, 
+    React.createElement('h1', {}, 'hello world')
+    );
+};
+*/
+
+//Attaching the component to the HTML interface.
+ReactDom.render(<Greeting/>, document.getElementById('root'));
