9424bcc5823bdba6c7d7fa3c114f2f95aceb46ff
AnawaratneMANA <it19086408@my.sliit.lk>
2021-04-17 04:22:23 -0400

    Calling method on clickEvents

--- src/index.js
+++ src/index.js
@@ -59,6 +59,16 @@ function BookList() {
   );
 }
 
+// Normal method get called upon the click event
+const clickHandler = () => {
+  alert('Hello World');
+}
+
+//Method with parameters -> when calling its different than usual.
+const clickHandlerParam = (brand) => {
+  console.log(brand);
+}
+
 //Adding different parameters to the book component
 const Book = (props) => {
   //This is called object distruturing in JavaScript.
@@ -69,6 +79,8 @@ const Book = (props) => {
       <h1>{manfacture}</h1>
       <h4>{model} </h4>
       <h4>{status}</h4>
+      <button type = 'button' onClick ={clickHandler}>Order</button> <br/>
+      <button type = 'button' onClick ={() => clickHandlerParam(manfacture)}>Get the Brand</button>
     </article>
   )
 }
