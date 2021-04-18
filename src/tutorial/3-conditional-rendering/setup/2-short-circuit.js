c9c76980f531a6885267260780f1c49117f8f736
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-04-18 23:23:43 +0530

    Alternative Way of Displaying the content conditionally in React (Can't use If)

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,38 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
 
-  return <h2>short circuit</h2>;
+  const [isError, setIsError] = useState(false);
+
+  const firstValue = text || 'hello world'; //if above have a value it will return it, else return second one.
+  const secondValue = text && 'hello world';
+
+  return (<React.Fragment>
+    { /*
+      <h1>{firstValue}</h1>
+      <h1>Value: {secondValue}</h1>
+    */ }
+
+    <h1>{text || 'john doe'}</h1>
+    <button className = 'btn' onClick={() => setIsError(!isError)}>Toggle Error</button>
+    {isError && <h1> Error...</h1>}
+    {isError ? <p>There is an Error...</p>: <div>
+      <h2> There is no Error</h2>
+    </div>}
+  </React.Fragment>);
 };
 
 export default ShortCircuit;
+
+
+/**
+ *
+ * Following is not working.
+ * return <>
+ *     {if(){console.log('hello world')}} </>
+ *
+ *  This is a alternative way of use a conditional statements for the component visibility.
+    if can't use to display component conditionally we have to use this technique.
+ */
+
