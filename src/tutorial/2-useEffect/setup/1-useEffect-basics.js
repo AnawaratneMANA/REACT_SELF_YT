2378c98340a5fbb2172cef86babf02eaebc54ead
AnawaratneMANA <it19086408@my.sliit.lk>
2021-04-18 01:43:47 -0400

    useEffect usage and second parameter usage.

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -7,8 +7,12 @@ const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
   useEffect (() => {
     console.log('Effect Called');
-    document.title = `New Messages(${value})`;
-  })
+    //Apply condition to the hooks.
+    if(value > 1 ){
+      document.title = `New Messages(${value})`; 
+    }
+    
+  }, [value])
 
   console.log("render component")
   return (<React.Fragment>
@@ -23,4 +27,11 @@ export default UseEffectBasics;
   This is use to change something outside the comopnent on changes made to the same compoenent.
   This can be used to message passing. And other communication procedures across the mutlli components.
 
+  How to conditionally call hooks.
+
+  15 line second parameter. if we add empty [] it will run only on the initial render
+  With a empty bracket it work like a constructor.
+  adding more values in to the second parameters.
+
+  when we change the value it will trigger the useEffects again and again.
 */
