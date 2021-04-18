1c1ca0820a97cb1cb9c8185b25114df518e2939c
AnawaratneMANA <it19086408@my.sliit.lk>
2021-04-18 00:40:21 -0400

    Changing the attributes with objects

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  //State Objects
+  const [person, setPerson] = useState({name: 'peter', age: 24, message: 'random message',})
+  console.log(person);
+
+  //State values
+  const [name, setName] = useState('peter')
+  const [age, setAge] = useState(24);
+  const [message, setMessaage] = useState('random title');
+
+  const changeMessage = () => {
+    //Change the Object values.
+    setPerson({...person, message: 'hello world'});
+    //Change the state values.
+    setMessaage('Hello World');
+  };
+
+
+  return (<React.Fragment>
+    <h3>{person.name}</h3>
+    <h3>{person.age}</h3>
+    <h3>{person.message}</h3>
+    <button className='btn' onClick={changeMessage}></button>
+  </React.Fragment>);
 };
 
 export default UseStateObject;
+
+/*
+without spread operator other data other than message will be 
+eraced out. 
+*/
