06e401c0b37f47152153f17cab48ca414fc71e17
AnawaratneMANA <it19086408@my.sliit.lk>
2021-04-18 00:12:08 -0400

    Using Hook create a simple name toggle function

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,31 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState("Hello World"));
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(value, handler)
+  const [text, setText] = useState('random title');//First one keyword second one method name.
+  const handleClick = () => {
+    //Toggling mechnism.
+    if(text === "random title"){
+      setText('Hello World');
+    } else {
+      setText("random title");
+    }
+    
+  }
+  return ( <React.Fragment>
+    <h1>{text}</h1>
+    <butotn className="btn" onClick= {handleClick} >Change the Name</butotn>
+  </React.Fragment>);
 };
 
 export default UseStateBasics;
+
+/*
+
+When working and creating hooks it's better to have use keyword infront of 
+it should be in a component which name start with capital letter.
+
+*/
