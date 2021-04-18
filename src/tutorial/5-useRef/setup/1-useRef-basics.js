405b26af04b33b83f1d8ba18043ec398242c6f88
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-04-19 00:55:29 +0530

    Connecting DOM elements to the React Component internal using useRef.

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -5,7 +5,32 @@ import React, { useEffect, useRef } from 'react';
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+
+  const refContainer = useRef(null);
+  const devContainer = useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  };
+
+  useEffect(()=> {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  })
+  console.log(refContainer);
+  return <React.Fragment>
+    <form className='form' onSubmit={handleSubmit}>
+      <div>
+        <input type='text' ref={refContainer}/>
+        <button type='submit'>Submit</button>
+      </div>
+    </form>
+    <div ref={devContainer}> Hello World</div>
+  </React.Fragment>;
 };
 
 export default UseRefBasics;
+
+/*
+* This is used to connect DOM element to the internals
+* */
