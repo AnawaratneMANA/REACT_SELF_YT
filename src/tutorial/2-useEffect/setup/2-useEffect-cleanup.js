0eafb54f0a6b03bc4cbeeced253d19a4ffe73637
AnawaratneMANA <it19086408@my.sliit.lk>
2021-04-18 05:24:26 -0400

    useEffect and Browser window width updating

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,32 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  console.log(size)
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize);
+
+    //Adding a return function
+    return () => {
+      console.log('Cleaning Up...');
+      window.removeEventListener('resize', checkSize);
+    }
+  }, [])
+
+
+
+  return (<React.Fragment>
+    <h1> Window</h1>
+    <h2> {size} PX</h2>
+  </React.Fragment>);
+
 };
 
 export default UseEffectCleanup;
+
+//Stop at 5.10
