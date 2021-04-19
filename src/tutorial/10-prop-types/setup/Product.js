d0e1f1bd26bbd89257ab1308a785495c2ef8b37f
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-04-19 16:41:44 +0530

    PropsTypes and alternative way of React without using Props

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,29 @@
 import React from 'react';
-
-const Product = () => {
-  return <article className='product'>single product</article>;
+import PropType from 'prop-types'
+//Importing the image
+import defaultImage from '../../../assets/default-image.jpeg'
+const Product = ({image, name, price}) => {
+  //Only if the image exist we are checking for the URL
+  const url = image && image.url;
+  return (
+      <article className = 'product'>
+        <h4>Single Product</h4>
+        <img src={url || defaultImage} alt={name}/>
+        <h4>{name}</h4>
+        <p>${price || 3.99}</p>
+      </article>
+  );
 };
 
+Product.propTypes = {
+  image: PropType.object.isRequired,
+  name: PropType.string.isRequired,
+  price: PropType.number.isRequired,
+
+};
+// Product.defaultProps = {
+//   name: 'default name',
+//   price: 3.99,
+//   image: defaultImage
+// }
 export default Product;
