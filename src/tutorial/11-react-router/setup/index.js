283ca1b76658084614c56ff1679e779e9efc4a2b
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-04-19 17:44:05 +0530

    Using react-router create detailed pages for details without creating a page.

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -24,6 +24,9 @@ const ReactRouterSetup = () => {
           <Route path='/people'>
             <People/>
           </Route>
+          <Route path = '/person/:id' children={<Person/>}>
+
+          </Route>
           <Route path="*">
             <Error/>
           </Route>
