135252905ff94adb8be4ce2c9b577183d93f2b09
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-04-22 08:36:29 +0530

    Added all the component in to the view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from "@material-ui/core";
-
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 //Importing the Image.
 import memories from './images/far_cry.png'
 const App = () => {
@@ -12,7 +13,13 @@ const App = () => {
             </AppBar>
             <Grow in>
                 <Grid container justify= "space-between" alignItems= "stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts/>
+                    </Grid>
 
+                    <Grid item xs={12} sm={4}>
+                        <Form/>
+                    </Grid>
                 </Grid>
             </Grow>
         </Container>
