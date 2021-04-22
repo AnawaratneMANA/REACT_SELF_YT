10a48b5dbd129346e2d1810675542f3679f41fe9
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-04-22 10:46:17 +0530

    Added useEffects Changers to the App.js File with Hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from "@material-ui/core";
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
 //Importing the Image.
 import memories from './images/far_cry.png'
 import useStyle from './styles';
 const App = () => {
     const classes = useStyle();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch])
     return (
         <Container maxWidth={"lg"}>
             <AppBar  className={classes.appBar} position= "static" color = "inherit">
