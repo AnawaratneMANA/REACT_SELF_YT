949b8e9400855672058cabba9ea6052b55219202
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-04-22 11:08:19 +0530

    Before creating the form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TextField, Button, Typography, Paper} from "@material-ui/core";
 import useStyle from './styles';
 const Form = () => {
     const classes = useStyle();
