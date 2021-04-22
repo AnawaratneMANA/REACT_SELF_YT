22107aa8d7a640ef1a37b32dd978aa79455c2397
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-04-22 11:27:13 +0530

    Adding Filebase64 file attachments to the form works everything

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { TextField, Button, Typography, Paper} from "@material-ui/core";
 import useStyle from './styles';
+import FileBase from 'react-file-base64';
 const Form = () => {
     const classes = useStyle();
     const  [postData, setPostData] = useState({
@@ -24,6 +25,38 @@ const Form = () => {
                            value={postData.creator}
                            onChange={(e) => setPostData({...postData, creator: e.target.value})}>
                 </TextField>
+
+                <TextField name= "title"
+                           variant= "outlined"
+                           label ="Title"
+                           fullWidth
+                           value={postData.title}
+                           onChange={(e) => setPostData({...postData, title: e.target.value})}>
+                </TextField>
+
+                <TextField name= "message"
+                           variant= "outlined"
+                           label ="Message"
+                           fullWidth
+                           value={postData.message}
+                           onChange={(e) => setPostData({...postData, message: e.target.value})}>
+                </TextField>
+
+                <TextField name= "tags"
+                           variant= "outlined"
+                           label ="Tags"
+                           fullWidth
+                           value={postData.tags}
+                           onChange={(e) => setPostData({...postData, tags: e.target.value})}>
+                </TextField>
+
+                <div className={classes.fileInput}>
+                    <FileBase
+                        type ="file"
+                        multiple={false}
+                        onDone={({base64}) => setPostData({...postData, selectedFile: base64})}
+                    />
+                </div>
             </form>
         </Paper>
     )
