025dd493005ae85ecfde1bfd75344563f4b30a0c
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-04-22 11:20:58 +0530

    Added the functionality to onChnage in Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,7 +3,13 @@ import { TextField, Button, Typography, Paper} from "@material-ui/core";
 import useStyle from './styles';
 const Form = () => {
     const classes = useStyle();
-    const  [postData, setPostData] = useState();
+    const  [postData, setPostData] = useState({
+        creator: '',
+        title: '',
+        message: '',
+        tags: '',
+        selectedFile: ''
+    });
     const handleSubmit = () => {
 
     }
@@ -12,7 +18,7 @@ const Form = () => {
             <form autoComplete ="off" noValidate className={classes.form} onSubmit={handleSubmit}>
                 <Typography variant={h6}> Creating a Memory</Typography>
                 <TextField name= "creator" variant= "outlined" label ="Creator" fullWidth
-                value={postData.creator} onChange={}>
+                value={postData.creator} onChange={(e) => setPostData({...postData, creator: e.target.value})}>
 
                 </TextField>
             </form>
